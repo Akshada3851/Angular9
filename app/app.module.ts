822dca2087af164a0665ad9088eed4a53d2fb8df
Akshada3851 <78719014+Akshada3851@users.noreply.github.com>
2021-03-23 00:22:02 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FirstComponent } from './app.component';
+
+@NgModule({
+  declarations: [FirstComponent],
+  imports: [BrowserModule],
+  bootstrap: [FirstComponent],
+})
+export class FirstModule{}
